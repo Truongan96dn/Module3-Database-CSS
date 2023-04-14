9e71b3612ecdea113820c2f19a0e51dc31b367f0
Truongan96dn <hotruongandn@gmail.com>
2023-04-14 09:18:01 +0700

    add casestudy

--- case study/task4(2-5).sql
+++ case study/task4(2-5).sql
@@ -14,5 +14,29 @@ JOIN loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
 WHERE  loai_khach.ten_loai_khach = "Diamond"
 group by ma_khach_hang
 order by so_lan_dat_phong;
+-- SQL 5
+SELECT
+    khach_hang.ma_khach_hang,
+    khach_hang.ho_ten AS ho_ten_khach_hang,
+    loai_khach.ten_loai_khach,
+    hop_dong.ma_hop_dong,
+    dich_vu.ten_dich_vu,
+    hop_dong.ngay_lam_hop_dong,
+    hop_dong.ngay_ket_thuc,
+    (ifnull((hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia) , 0)  + dich_vu.chi_phi_thue) AS tong_so_tien
+FROM
+    khach_hang
+        JOIN
+    loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+        LEFT JOIN
+    hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+        LEFT JOIN
+    dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+ORDER BY khach_hang.ma_khach_hang;
+
 
         
